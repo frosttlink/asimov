292f6be5914dc33353271ca39ceb478f4bf24ef9
LeoClink77 <leolorena.alves@gmail.com>
2026-04-21 21:30:14 -0300

    first commit

---
+++ parte-1/src/app.tsx
@@ -0,0 +1,9 @@
+import { Hero } from "./components/sections/Hero";
+
+export function App() {
+  return (
+    <div className="px-25 py-15 gap-17.5">
+      <Hero />
+    </div>
+  )
+}
---
+++ parte-1/src/components/ui/Button.tsx
@@ -0,0 +1,37 @@
+import type { ComponentProps, ReactNode } from "react"
+
+type ButtonVariant = "primary" | "secondary" | "tertiary"
+
+interface ButtonProps extends ComponentProps<"button"> {
+  children: ReactNode
+  variant?: ButtonVariant
+}
+
+export function Button({
+  children,
+  variant = "primary",
+  className = "",
+  ...props
+}: ButtonProps) {
+  const base =
+    "flex items-center justify-center gap-2.5 rounded-[14px] font-normal transition-all duration-200 px-8.75 py-5 h-17 cursor-pointer"
+
+  const variants: Record<ButtonVariant, string> = {
+    primary:
+      "bg-dark text-white border border-dark",
+    secondary:
+      "bg-transparent text-black border border-dark",
+    tertiary:
+      "bg-green text-black border border-green ",
+  }
+
+  return (
+    <button
+      type="button"
+      className={`${base} ${variants[variant]} ${className}`}
+      {...props}
+    >
+      {children}
+    </button>
+  )
+}
