5c7585bd9ad12c216364442a931430c40f963f4a
LeoClink77 <leolorena.alves@gmail.com>
2026-04-23 19:00:43 -0300

    fix: correct swiper css import

--- parte-1/src/components/sections/studies-cases.tsx
+++ parte-1/src/components/sections/studies-cases.tsx
@@ -30,7 +30,7 @@ export function StudiesCases() {
           >
 
             <SwiperSlide className="h-full">
-              <div className="flex flex-col justify-between gap-5 bg-dark rounded-[45px] px-6 py-8 h-[260px] sm:h-[280px]">
+              <div className="flex flex-col justify-between gap-5 bg-dark rounded-[45px] px-6 py-8 h-65 sm:h-70">
                 <p className="text-white text-p leading-[130%]">
                   For a local restaurant, we implemented a targeted PPC campaign that resulted in a 50% increase in website traffic and a 25% increase in sales.
                 </p>
@@ -39,7 +39,7 @@ export function StudiesCases() {
             </SwiperSlide>
 
             <SwiperSlide className="h-full">
-              <div className="flex flex-col justify-between gap-5 bg-dark rounded-[45px] px-6 py-8 h-[260px] sm:h-[280px]">
+              <div className="flex flex-col justify-between gap-5 bg-dark rounded-[45px] px-6 py-8 h-65 sm:h-70">
                 <p className="text-white text-p leading-[130%]">
                   For a B2B software company, we developed an SEO strategy that resulted in a first page ranking for key keywords and a 200% increase in organic traffic.
                 </p>
@@ -48,7 +48,7 @@ export function StudiesCases() {
             </SwiperSlide>
 
             <SwiperSlide className="h-full">
-              <div className="flex flex-col justify-between gap-5 bg-dark rounded-[45px] px-6 py-8 h-[260px] sm:h-[280px]">
+              <div className="flex flex-col justify-between gap-5 bg-dark rounded-[45px] px-6 py-8 h-65 sm:h-70">
                 <p className="text-white text-p leading-[130%]">
                   For a national retail chain, we created a social media marketing campaign that increased followers by 25% and generated a 20% increase in online sales.
                 </p>
